561d89cf4b79d9533b1eb3c9ad459b1c6e86e5c5
Alan Edlebeck <alan.edlebeck@gmail.com>
2022-06-02 16:48:42 -0500

    home routes working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -55,14 +55,15 @@ router.get('/post/:id', (req, res) => {
     },
     attributes: [
       'id',
-      'post_url',
       'title',
-      'created_at',
+      'text',
+      'user_id',
+      'created_at'
     ],
     include: [
       {
         model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        attributes: ['id', 'comment_text', 'user_id', 'post_id', 'created_at'],
         include: {
           model: User,
           attributes: ['username']
@@ -79,6 +80,7 @@ router.get('/post/:id', (req, res) => {
         res.status(404).json({ message: 'No post found with this id' });
         return;
       }
+      console.log(dbPostData);
 
       // serialize the data
       const post = dbPostData.get({ plain: true });
